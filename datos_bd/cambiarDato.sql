bfd586b24e0ea721327443b100741cecee8126d0
Marcos <marcos23589@gmail.com>
2021-12-07 18:53:14 -0300

    version beta 2

--- datos_bd/cambiarDato.sql
+++
@@ -1,4 +0,0 @@
-set @documento = 42250197;
-select @alumno:= idAlumnos from alumnos where documento = @documento;
-update legajo set libro= 3, folio=92 where legajo.idLegajo = @alumno;
-
--- datos_bd/users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `users` (
-  `idUsers` int NOT NULL AUTO_INCREMENT,
-  `user` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`idUsers`));
